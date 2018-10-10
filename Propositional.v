d2d9b1c54e8ebf886678440434d0d8d9b3fee392
Georgy Dunaev <georgedunaev@gmail.com>
2018-10-10 17:52:27 +0300

    Kripke

--- Propositional.v
+++ Propositional.v
@@ -22,10 +22,11 @@ Inductive Fo :=
  |Impl:Fo->Fo->Fo
 .
 
-Notation " x --> y ":=(Impl x y) (at level 80).
+Notation " x --> y ":=(Impl x y) (at level 80, right associativity). 
+(*81, right associativity*)
 Notation " x -/\ y ":=(Conj x y) (at level 80).
 Notation " x -\/ y ":=(Disj x y) (at level 80).
-
+Notation " -. x " :=(Impl x Bot) (at level 80).
 (* Substitution *)
 Fixpoint subPF (t:Fo) (xi: PropVars.t) (u : Fo): Fo.
 Proof.
@@ -59,7 +60,16 @@ Fixpoint foI (f : Fo) : Omega :=
 Inductive PROCA : Fo -> Type :=
 | Ha1  : forall A B, PROCA (A-->(B-->A))
 | Ha2  : forall A B C, PROCA ((A-->(B-->C))-->((A-->B)-->(A-->C)))
+| Ha3  : forall A B, PROCA ((A-/\ B)--> A)
+| Ha4  : forall A B, PROCA ((A-/\ B)--> B)
+| Ha5  : forall A B, PROCA (A-->(B-->(A-/\B)))
+| Ha6  : forall A B, PROCA (A-->(A-\/ B))
+| Ha7  : forall A B, PROCA (B-->(A-\/ B))
+| Ha8  : forall A B C, PROCA ((A-->C)-->((B-->C)-->((A-\/ B)-->C)))
+| Ha9  : forall A B, PROCA (-.A --> A --> B )
 .
+(*Check Ha9.*)
+
 Section PR.
 Context (ctx:list Fo).
 Context (axs:Fo -> Type).
@@ -145,6 +155,18 @@ apply H4.
   exact H4.
 simpl in * |- *.
 admit.*)
+  * simpl. intros. destruct H0 as [LH0 RH0]. exact LH0.
+  * simpl. intros. destruct H0 as [LH0 RH0]. exact RH0.
+  * simpl. intros x y pxy yA z pyz zB. split.
+    exact (utv2 y z A yA pyz).
+    exact zB.
+  * simpl. intros x y pxy H. left. exact H.
+  * simpl. intros x y pxy H. right. exact H.
+  * simpl. intros.
+    destruct H4.
+    - unshelve eapply H0. 2: exact H4. exact (R_transitive y y0 y1 H1 H3).
+    - unshelve eapply H2. exact H3. exact H4.
+  * simpl. intros. exfalso. eapply H0 with y0. exact H1. exact H2.
 + simpl in * |- *.
   intro x.
   unshelve apply (IHPR2 x).
@@ -184,12 +206,149 @@ Inductive SubFo (f:Fo): Fo -> Type :=
 | sfir : forall (g1 g2 : Fo), (SubFo f g2) -> (SubFo f (g1 --> g2))
 .
 
-Lemma lem1 G D s g (J1:SubFo s g) 
+Definition AtoA {ctx} (A:Fo) : PR ctx PROCA (A-->A).
+Proof.
+apply MP with (A-->(A-->A)).
+apply Hax, Ha1. (* apply (Hax _ _ (Ha1 _ _)).*)
+apply MP with (A-->((A-->A)-->A)) (*1:=I*).
+apply Hax, Ha1.
+apply Hax, Ha2.
+Defined.
+
+
+(*Fixpoint *)
+Theorem weak (axs:Fo -> Type)
+(A F:Fo) (l :list Fo) (x: (PR l axs F)) : (PR (A::l) axs F).
+Proof.
+induction x.
++ apply hyp.
+  right.
+  exact i.
++ apply Hax, a.
++ exact (MP (A::l) axs A0 B IHx1 IHx2).
+Defined.
+
+Fixpoint weaken (F:Fo) (li l :list Fo) (x: (PR l PROCA F)) {struct li}:
+ (PR (li ++ l) PROCA F).
+Proof.
+destruct li.
+simpl.
+exact x.
+simpl.
+simple refine (@weak _ f F (li ++ l)%list _).
+apply weaken.
+exact x.
+Defined.
+
+Export Coq.Lists.List.
+
+Definition neg (f:Fo):= (Impl f Bot).
+
+Definition a1i (A B : Fo)(l : list Fo):
+(PR l PROCA B)->(PR l PROCA (Impl A B)).
+Proof.
+intros x.
+apply MP with (A:= B).
+exact x.
+eapply (*subcalc_OE,*) Hax,Ha1.
+Defined.
+
+(* Deduction *)
+Theorem Ded (A B:Fo)(il:list Fo)(m:(PR (A::il) PROCA B)) 
+:(PR il PROCA (A-->B)).
+Proof.
+induction m.
++ unfold InL in i.
+  simpl in i .
+  destruct i .
+  * rewrite <- e.
+    pose (J:=weaken _ il [] (AtoA A )).
+    rewrite app_nil_r in J.
+    exact J.
+  * simpl in i.
+    apply a1i.
+    apply hyp with (ctx:=il) (1:=i).
++ apply a1i.
+  apply Hax, a.
++ apply MP with (A-->A0).
+  exact IHm1.
+  apply MP with (A-->A0-->B).
+  exact IHm2.
+  apply Hax.
+  apply Ha2.
+Defined.
+
+Theorem invDed (A B:Fo)(il:list Fo)(m:(PR il PROCA (A-->B)))
+:(PR (A::il) PROCA B).
+Proof.
+pose(U:=(weak PROCA A _ il m)).
+assert (N:PR (A :: il) PROCA A).
+apply hyp. simpl. left. reflexivity.
+apply MP with A.
+exact N.
+exact U.
+Defined.
+
+(* Order of the context is not important. *)
+Lemma permut axs L1 L2 A (H: forall x, InL x L1 -> InL x L2)
+: (PR L1 axs A) -> (PR L2 axs A).
+Proof.
+intro m.
+induction m.
++ apply hyp. apply (H A i).
++ apply Hax. apply a.
++ apply MP with A. exact IHm1. exact IHm2.
+Defined.
+
+(* Both are inconsistent then (G,D) is inconsistent*)
+Lemma lem1_0 G D s g (J1:SubFo s g) 
 (a1: incpa (s :: G) D )
 (a2: incpa G (s :: D) ) : incpa G D.
 Proof.
 unfold incpa in * |- *.
 simpl in * |- *.
+apply Ded.
+apply MP with (s-\/(DISJ D)).
++ apply invDed in a2.
+  exact a2.
++ apply MP with (DISJ D --> DISJ D).
+  apply AtoA.
+  apply MP with (s --> DISJ D).
+  2 : {apply Hax. apply Ha8. }
+  pose (r:=Hax [] PROCA _ (Ha5 s (CONJ G))).
+  apply invDed in r.  apply invDed in r.
+  apply weak with (A:=s) in a1.
+  apply weak with (A:=CONJ G) in a1.
+  assert (K:PR [CONJ G;s] PROCA (DISJ D)).
+  apply MP with (A:=s-/\ CONJ G).
+  exact r. exact a1.
+  apply Ded.
+  apply permut with (L1:=[CONJ G;s]).
+  2 : exact K.
+  simpl.
+  firstorder.
+Defined.
+Locate prod.
+Print Scopes.
+Open Scope type_scope.
+
+Section Completeness.
+Context (phi:Fo).
+
+(*
+Lemma lem1 (G D:list Fo) (J:incpa G D) : 
+exists GG DD:list Fo, (incpa GG DD) * 
+(forall x, InL x G -> InL x GG) *
+(forall x, InL x D -> InL x DD).
+UNIVERSE INCONSISTENCY
+*)
+
+assert (asse:PR [CONJ G] PROCA (s -\/ DISJ D)).
+admit.
+simpl in asse.
+pose (Q:= Hax [] PROCA _ (Ha5 s (CONJ G))).
+
+destruct asse.
 Abort.
 
 End WR.
